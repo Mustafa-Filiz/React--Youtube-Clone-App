f256ee93a63e5eed43959ec220f2daffa2e3fc99
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-26 17:44:38 +0300

    updates

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,17 +1,32 @@
-import { Box } from '@mui/system'
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { videosSelector } from '../redux/youtubeSlice'
+import { Paper, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { videosSelector } from '../redux/youtubeSlice';
 
 function VideoPlayer() {
+    const video = useSelector(videosSelector).items[0];
 
-	const videos = useSelector(videosSelector)
-	console.log(videos)
-	return (
-		<Box>
-			
-		</Box>
-	)
+    return (
+        <Box>
+            {video && (
+                <Box>
+                    <iframe
+                        title={video.etag}
+                        src={`https://youtube.com/embed/${video.id.videoId}`}
+                        frameBorder="0"
+                        width="600"
+                        height="500"
+                    ></iframe>
+
+                    <Paper>
+						<Typography variant="h6">{video.snippet.title}</Typography>
+						<Typography variant="p">{video.snippet.description}</Typography>
+					</Paper>
+                </Box>
+            )}
+        </Box>
+    );
 }
 
-export default VideoPlayer
+export default VideoPlayer;
