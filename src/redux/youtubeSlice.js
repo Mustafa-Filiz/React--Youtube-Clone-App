4244b81245ab0ac3d99f9c579d5002567d10ac4d
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-26 16:59:34 +0300

    updates

---
+++ src/redux/youtubeSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const youtubeSlice = createSlice({
+    name: 'youtube',
+    initialState: {},
+    reducers: {},
+    extraReducers: {},
+});
+
+export default youtubeSlice.reducer;
