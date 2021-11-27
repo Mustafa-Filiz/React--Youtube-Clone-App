119650f300274b273cb8cac288f5cb79b3631ee2
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-27 17:02:16 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -4,19 +4,16 @@ import Navbar from './components/Navbar';
 import VideoList from './components/VideoList';
 import VideoPlayer from './components/VideoPlayer';
 import { useStyles } from './styles/styles';
-import ThemeProviderContext from './theme/ThemeProvider';
 
 function App() {
     const classes = useStyles();
     return (
         <Box>
-            <ThemeProviderContext>
-                <Navbar />
-                <Box className={classes.container}>
-                    <VideoPlayer />
-                    <VideoList />
-                </Box>
-            </ThemeProviderContext>
+            <Navbar />
+            <Box className={classes.container}>
+                <VideoPlayer />
+                <VideoList />
+            </Box>
         </Box>
     );
 }
