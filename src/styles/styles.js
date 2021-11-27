ba5814fba2998082cad1c81066be10e699316cb1
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-27 17:02:49 +0300

    updates

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,25 +1,32 @@
 import { makeStyles } from '@mui/styles';
 
 export const useStyles = makeStyles((theme) => {
-    console.log(theme);
     return {
         container: {
             display: 'flex',
             justifyContent: 'space-between',
             margin: '2rem auto',
             width: '90%',
+            [theme.breakpoints.down(930)]: {
+                flexDirection: 'column',
+            },
         },
         videoPlayer: {
             width: '45%',
-            [theme.breakpoints.down('md')]: {
-                width: '100%',
-              },
+            [theme.breakpoints.down(930)]: {
+                width: '90%',
+            },
+        },
+        videoTitle: {
+            padding: '1rem',
+            marginTop: '0.5rem',
         },
         listContainer: {
             width: '40%',
-            [theme.breakpoints.down('md')]: {
-                width: '100%',
-              },
+            [theme.breakpoints.down(930)]: {
+                width: '90%',
+                marginTop: '5rem',
+            },
         },
         listItem: {
             display: 'flex',
@@ -29,6 +36,7 @@ export const useStyles = makeStyles((theme) => {
             cursor: 'pointer',
             '&:hover': {
                 textDecoration: 'underline',
+                backgroundColor: '#eee',
             },
         },
     };
